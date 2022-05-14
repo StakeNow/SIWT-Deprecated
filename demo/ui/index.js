f79b0677c255280bf15ff4b8498d4616cac17ce5
Jeroen Branje <info@jeroenbranje.nl>
2022-05-14 19:38:37 +0200

    update design, messages

--- demo/ui/index.js
+++ demo/ui/index.js
@@ -15,19 +15,28 @@ const API_URL = process.env.API_URL || 'http://localhost:3000'
 
 const setBackground = (status) => {
   const DemoContainer = document.getElementsByClassName('demo-container')[0]
-  DemoContainer.classList.remove('from-sky-500', 'to-indigo-500')
+  // const DemoContainer = document.getElementsByClassName('message-container')[0]
+  // const DemoContainer = document.getElementsByClassName('message-content')[0]
+  DemoContainer.classList.remove('from-sky-500', 'to-sky-500')
+  DemoContainer.classList.remove('bg-blue-700')
   if (status === 200) {
     DemoContainer.classList.add('from-green-500', 'to-sky-500')
+    DemoContainer.classList.add('bg-green-500')
   } else if (status === 403) {
     DemoContainer.classList.add('from-red-500', 'to-sky-500')
+    DemoContainer.classList.add('bg-red-500')
   } else {
     DemoContainer.classList.add('from-sky-500', 'to-indigo-500')
+    DemoContainer.classList.add('bg-blue-700')
   }
 
   setTimeout(() => {
     DemoContainer.classList.remove('from-red-500', 'to-sky-500')
     DemoContainer.classList.remove('from-green-500', 'to-sky-500')
     DemoContainer.classList.add('from-sky-500', 'to-indigo-500')
+    DemoContainer.classList.remove('bg-green-500')
+    DemoContainer.classList.remove('bg-red-500')
+    DemoContainer.classList.add('bg-blue-700')
   }, 1000)
 }
 
@@ -43,12 +52,18 @@ const getProtectedData = () => {
       return response.json()
     })
     .then(data => {
-      const protectedDataContainer = document.getElementsByClassName('protected-data-content-container')[0]
-      protectedDataContainer.innerHTML = data
+      // const protectedDataContainer = document.getElementsByClassName('protected-data-content-container')[0]
+      // protectedDataContainer.innerHTML = data
+
+      const MessageContent = document.getElementsByClassName('message-content')[0]
+      MessageContent.innerHTML = data
     })
     .catch(error => {
-      const protectedDataContainer = document.getElementsByClassName('protected-data-content-container')[0]
-      protectedDataContainer.innerHTML = error.message
+      // const protectedDataContainer = document.getElementsByClassName('protected-data-content-container')[0]
+      // protectedDataContainer.innerHTML = error.message
+
+      const MessageContent = document.getElementsByClassName('message-content')[0]
+      MessageContent.innerHTML = error.message
     })
 }
 
@@ -64,12 +79,18 @@ const getSignedInData = () => {
       return response.json()
     })
     .then(data => {
-      const protectedDataContainer = document.getElementsByClassName('signed-in-data-content-container')[0]
-      protectedDataContainer.innerHTML = data
+      // const protectedDataContainer = document.getElementsByClassName('signed-in-data-content-container')[0]
+      // protectedDataContainer.innerHTML = data
+
+      const MessageContent = document.getElementsByClassName('message-content')[0]
+      MessageContent.innerHTML = data
     })
     .catch(error => {
-      const protectedDataContainer = document.getElementsByClassName('signed-in-data-content-container')[0]
-      protectedDataContainer.innerHTML = error.message
+      // const protectedDataContainer = document.getElementsByClassName('signed-in-data-content-container')[0]
+      // protectedDataContainer.innerHTML = error.message
+
+      const MessageContent = document.getElementsByClassName('message-content')[0]
+      MessageContent.innerHTML = error.message
     })
 }
 
@@ -82,12 +103,18 @@ const getPublicData = () => {
       return response.json()
     })
     .then(data => {
-      const publicDataContainer = document.getElementsByClassName('public-data-content-container')[0]
-      publicDataContainer.innerHTML = data
+      // const publicDataContainer = document.getElementsByClassName('public-data-content-container')[0]
+      // publicDataContainer.innerHTML = data
+
+      const MessageContent = document.getElementsByClassName('message-content')[0]
+      MessageContent.innerHTML = data
     })
     .catch(error => {
-      const publicDataContainer = document.getElementsByClassName('public-data-content-container')[0]
-      publicDataContainer.innerHTML = error.message
+      // const publicDataContainer = document.getElementsByClassName('public-data-content-container')[0]
+      // publicDataContainer.innerHTML = error.message
+
+      const MessageContent = document.getElementsByClassName('message-content')[0]
+      MessageContent.innerHTML = error.message
     })
 }
 
@@ -125,7 +152,7 @@ const login = async () => {
 
     if (idToken) {
       const userIdInfo = jwt_decode(idToken)
-      contentContainer.innerHTML = `<h3>You are logged in as ${userIdInfo.pkh}</h3>`
+      contentContainer.innerHTML = `<h3>You are connected with <br />${userIdInfo.pkh}</h3>`
     }
   } catch (error) {
     const contentContainer = document.getElementsByClassName('content-container')[0]
